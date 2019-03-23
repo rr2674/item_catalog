f48e9076183d785a5a5a0ecfe5bab2a712e44bd8
Bob Roycroft <rr2674@att.com>
2019-03-23 15:14:40 -0500

    Cancel button add-edit html; css updates

---
+++ static/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  console.log('here2')
+  $("#btn-delete").click(function() {
+    console.log('here3')
+// TODO:     $('input, textarea').removeAttr('required')
+    //e.preventDefault(); // cancel submission
+    //window.location.replace("{{url_for('showCatalog')}}");
+    // TODO: return false
+  });
+});
