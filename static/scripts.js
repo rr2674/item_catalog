9b5f68b766ea785d65169caf680daacf9cd33708
Bob Roycroft <rr2674@att.com>
2019-03-23 21:36:31 -0500

    Vertical tab html template with styles

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,21 @@
-$(document).ready(function() {
-  console.log('here2')
-  $("#btn-delete").click(function() {
-    console.log('here3')
-// TODO:     $('input, textarea').removeAttr('required')
-    //e.preventDefault(); // cancel submission
-    //window.location.replace("{{url_for('showCatalog')}}");
-    // TODO: return false
+$(document).ready(function(){
+  console.log('here');
+  $("defaultOpen").click()
+});
+
+$(document).ready(function(){
+  console.log('here2');
+  $("button").click(function(){
+      console.log('clicked: ' + this.name);
+      $('.tabcontent').each(function(i, obj) {
+           console.log('hide obj: ' + obj.id);
+           $('#' + obj.id).hide();
+      });
+      $('.tablinks').each(function(i, obj) {
+           console.log('remove active class from obj: ' + obj.name);
+           $( 'button[name=' + this.name + ']' ).removeClass('active');
+      });
+      $( 'button[name=' + this.name + ']' ).addClass('active');
+      $('div[id=' + this.name + ']').show();
   });
 });
